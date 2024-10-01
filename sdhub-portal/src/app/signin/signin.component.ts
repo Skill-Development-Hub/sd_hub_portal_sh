10b41803701a2aca9bc5f478735b4e274b68ec2a
Shahriar Hussain <shahriarhus@gmail.com>
2024-10-01 12:12:56 +0530

    Updated signcomponent.ts

--- sdhub-portal/src/app/signin/signin.component.ts
+++ sdhub-portal/src/app/signin/signin.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
+  signInForm!: FormGroup;
 
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void{
+    this.signInForm = this.fb.group({
+      email: ['', [Validaters.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minlength(6)]],
+    });
+  }
+
+  OnSubmit(): void{
+    if(this.signInForm.valid) {
+      const {email, password} = this.signInForm.value;
+      console.log('Sign-In Data:', {email});
+    } else {
+      console.log("Validation Failed")
+    }
+  }
 }
