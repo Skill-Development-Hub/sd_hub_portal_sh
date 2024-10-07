cabd54be5eb18cbfe7f50939a58cc2e2cc5fa351
Shahriar Hussain <shahriarhus@gmail.com>
2024-10-07 11:54:16 +0530

    Generated Registration component & Fixes

--- sdhub-portal/src/app/app-routing.module.ts
+++ sdhub-portal/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: '',redirectTo: 'signin', pathMatch: "full"},
   { path: 'signin', component: SigninComponent},
+  { path: 'registration', component: RegistrationComponent},
 ];
 
 @NgModule({
--- sdhub-portal/src/app/registration/registration.component.ts
+++ sdhub-portal/src/app/registration/registration.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +11,13 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  private _formBuilder = inject(FormBuilder);
 
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 }
